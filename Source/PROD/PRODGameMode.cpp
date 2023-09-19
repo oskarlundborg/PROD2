0c83321a7f5354dab26a91cf6a650599f0a04cfa
Fenster <oskarlundborg@hotmail.com>
2023-09-19 09:08:05 +0200

    Initial Commit

---
+++ Source/PROD/PRODGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PRODGameMode.h"
+#include "PRODCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APRODGameMode::APRODGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
---
+++ Source/PROD/PRODGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PRODGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APRODGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APRODGameMode();
+};
+
+
+
